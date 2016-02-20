973f414e3341c801bdc0027cca569bbcf82ce879
Arnout Engelen <arnouten@bzzt.net>
2016-02-21 00:57:38 +0100

    Keep track of which branches we recommended to be updated

--- CheckBranches.scala
+++ CheckBranches.scala
@@ -5,12 +5,23 @@ object CheckBranches extends App {
     "basic03" -> "basic04",
     "basic04" -> "basic-final"
   )
-  
-  course.foreach {
-    case (givenBranch, solutionBranch) => 
-      if (!GitRepo.isParent(givenBranch, solutionBranch)) {
-        println(s"git checkout $solutionBranch && git merge -m 'propagate changes' $givenBranch")
-      }
-  } 
 
+  case class Analysis(
+    recommendedCommands: List[String] = Nil,
+    updatedBranches: Set[String] = Set())
+
+  val a = course.foldLeft(Analysis()) {
+    case (acc, (givenBranch, solutionBranch)) =>
+      if (!GitRepo.isParent(givenBranch, solutionBranch)) {
+        acc.copy(
+          recommendedCommands =
+            s"git merge -m 'propagate changes' $givenBranch"
+            :: s"git checkout $solutionBranch"
+            :: acc.recommendedCommands,
+          updatedBranches = acc.updatedBranches + solutionBranch
+        )
+      } else acc
+  }
+
+  println(a.recommendedCommands.reverse.mkString(" && "))
 }
